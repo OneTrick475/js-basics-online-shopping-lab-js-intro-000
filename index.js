b45224b8a68bc7aa257647e7687748ec59c53563
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-03 17:03:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-var itm = {itemName: 'item', itemPrice: `${Math.floor(Math.random() * 100)+1}`};
+var itm = {itemName: `${item}`, itemPrice: `${Math.floor(Math.random() * 100)+1}`};
 cart.push(itm);
 return `${item} has been added to your cart.`
 }
