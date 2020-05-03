d3bbcf34392a10c5ce12a6d35c9002b21a2d2581
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-03 19:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ function placeOrder(cardNumber) {
   }
   else{
     cart.splice(0);
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is ${j}, which will be charged to the card ${cardNumber}.`
   }
 }
